dbc22eb52ddd9a3dba51446c464a96a0676378c1
Yuqian Li <liyuqian@google.com>
2021-12-24 16:37:06 -0800

    Add more functions to _deps

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
-import { createSheet, onEdit, onOpen } from './sheet';
+import { archive, createSheet, filterDueSoon, onEdit, onOpen } from './sheet';
 import { testAll } from '../test/integration_test';
 
 function main() {
   createSheet();
 }
 
-// Functions testAll, onOpen and onEdit are needed but not explicitly called
-// inside main. So we create this function to introduce the dependencies. This
-// function is not expected to be called anywhere.
+// Functions archive, filterDueSoon, testAll, onOpen and onEdit are needed but
+// not explicitly called inside main. So we create this function to introduce
+// the dependencies. This function is not expected to be called anywhere.
 function _deps() {
-  var e = null;
   onOpen();
-  onEdit(e);
+  onEdit(null);
   testAll();
+  filterDueSoon(null);
+  archive(null);
 }
